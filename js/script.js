8fee97753fca5a9732febb38c37168e35a2ea442
BLemon7 <Benny@users-MacBook-Air.local>
2018-05-03 21:38:28 +0100

    takahe silhouette fade in

--- js/script.js
+++ js/script.js
@@ -1,64 +1,73 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-		// animate the anchor click
-		$('a[href^="#project"]').on('click', function(e) {
-				e.preventDefault();
+    // animate the anchor click
+    $('a[href^="#project"]').on('click', function (e) {
+        e.preventDefault();
 
-				var target = this.hash;
-				var $target = $(target);
+        var target = this.hash;
+        var $target = $(target);
 
-				$('html, body').stop().animate({
-						'scrollTop': $target.offset().top
-				}, 900, 'swing', function() {
-						window.location.hash = target;
-				});
-		});
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 
-		// when the page scrolls animate the scroll to top button to fade in or out
-		$(window).scroll(function() {
-				if ($(this).scrollTop() > 100) {
-						$('.scroll-to-top').fadeIn();
-				} else {
-						$('.scroll-to-top').fadeOut();
-				}
-		});
+    // when the page scrolls animate the scroll to top button to fade in or out
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
 
-		//click to scroll to top
-		$('.scroll-to-top').click(function() {
-				$('html, body').animate({
-						scrollTop: 0
-				}, 800);
-				return false;
-		});
+    //click to scroll to top
+    $('.scroll-to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
 
-	// hide .navbar first
-	$(".navbar.home").hide();
-	$(".opacity-layer").hide();
-	$('.opacity-layer').slideDown(1000, animateFirstText);
-		
+    // hide .navbar first
+    $(".navbar.home").hide();
+    $(".opacity-layer").hide();
+    $('.opacity-layer').slideDown(1000, animateFirstText);
 
-				
-	function animateFirstText(){
-		$('.slide:first').slideDown(200, animateSlideText);
-	}
-				
-	function animateSlideText(){
-		$(this).next('.slide').slideDown(500, animateSlideText);
-	}
-	
 
-	// fade in .navbar
-	$(function () {
-			$(window).scroll(function () {
 
-							 // set distance user needs to scroll before we start fadeIn
-					if ($(this).scrollTop() > 100) {
-							$('.navbar.home').fadeIn();
-					} else {
-							$('.navbar.home').fadeOut();
-					}
-			});
-	});
-		
-		
+    function animateFirstText() {
+        $('.slide:first').slideDown(200, animateSlideText);
+    }
+
+    function animateSlideText() {
+        $(this).next('.slide').slideDown(500, animateSlideText);
+    }
+
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+            // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 100) {
+                $('.navbar.home').fadeIn();
+            } else {
+                $('.navbar.home').fadeOut();
+            }
+        });
+    });
+
+
+});
+
+
+$(document).ready(function () {
+    $(".silhouette").click(function (e) {
+        $(".silhouette").fadeOut('slow', function () {
+            $(".the-takahe").fadeIn('slow');
+        });
+    });
 });
